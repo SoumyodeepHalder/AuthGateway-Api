a7b77206e135679cae45c56b7dda4d34c8660117
SoumyodeepHalder <92792771+SoumyodeepHalder@users.noreply.github.com>
2026-08-08 12:55:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
 
 // Allow all cross-origin requests 
 app.use(cors()); 
